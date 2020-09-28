ec7b311396adbb4deb1abce99261e4f4b721d215
Joshua Gato <joshuagato37@gmail.com>
2020-09-28 12:15:02 +0000

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BBcom</title>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <title>BBcom</title>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
