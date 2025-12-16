454318a14b7c3d76434de2df544e4b5216b854a7
Joshua Gato <joshuagato37@gmail.com>
2025-12-16 08:42:28 +0000

    Used env variable for image url

--- resources/js/components/HomePage/ViewedProducts/ViewedProducts.vue
+++ resources/js/components/HomePage/ViewedProducts/ViewedProducts.vue
@@ -1,252 +1,295 @@
 <template>
-  <section id="viewed-products" ref="viewed-products">
-    <section class="section-heading pb-5">
-      <h3>Viewed Products</h3>
-      <span v-if="viewedproducts && (viewedproducts.length > 0)">
-        You've viewed these products recently.
-      </span>
-      <span v-if="!viewedproducts">
-        You've not yet viewed any product.
-      </span>
+    <section id="viewed-products" ref="viewed-products">
+        <section class="section-heading pb-5">
+            <h3>Viewed Products</h3>
+            <span v-if="viewedproducts && viewedproducts.length > 0">
+                You've viewed these products recently.
+            </span>
+            <span v-if="!viewedproducts">
+                You've not yet viewed any product.
+            </span>
+        </section>
+        <template>
+            <section
+                class="carousel-section"
+                v-if="viewedproducts && viewedproducts.length > 0"
+            >
+                <carousel v-bind="settings">
+                    <div
+                        ref="viewed"
+                        tabindex="0"
+                        class="viewed activeviewed"
+                        v-for="prod in viewedproducts"
+                        :key="prod.id"
+                    >
+                        <div class="img-container">
+                            <img
+                                :src="
+                                    `${imageUrl}${getAttribute(
+                                        prod.product,
+                                        'image'
+                                    )}`
+                                "
+                                alt="img-url"
+                            />
+                        </div>
+                        <div class="details-container">
+                            <p class="product-category text-center mb-1">
+                                <span class="pl-1">{{
+                                    fetchOneCategoryMethod(
+                                        getAttribute(prod.product, "category")
+                                    )
+                                }}</span>
+                            </p>
+                            <h6 class="product-name text-center">
+                                {{ getAttribute(prod.product, "title") }}
+                            </h6>
+                            <p class="product-price text-center">
+                                ${{ getAttribute(prod.product, "price") }}
+                            </p>
+                            <button type="button">Add To Cart</button>
+                        </div>
+                    </div>
+                </carousel>
+            </section>
+            <section v-if="!viewedproducts">
+                <div ref="viewed" tabindex="0" class="viewed">
+                    <br class="line-break" />
+                </div>
+            </section>
+        </template>
     </section>
-    <template>
-      <section class="carousel-section" v-if="viewedproducts && viewedproducts.length > 0">
-        <carousel v-bind="settings">
-          <div ref="viewed" tabindex="0" class="viewed activeviewed" v-for="prod in viewedproducts" :key="prod.id" >
-            <div class="img-container">
-              <img :src="`${imageUrl}${getAttribute(prod.product, 'image')}`" alt="img-url">
-            </div>
-            <div class="details-container">
-              <p class="product-category text-center mb-1">
-                <span class="pl-1">{{ fetchOneCategoryMethod(getAttribute(prod.product, 'category')) }}</span>
-              </p>
-              <h6 class="product-name text-center">{{ getAttribute(prod.product, 'title') }}</h6>
-              <p class="product-price text-center">${{ getAttribute(prod.product, 'price') }}</p>
-              <button type="button">Add To Cart</button>
-            </div>
-          </div>
-        </carousel>
-      </section>
-      <section v-if="!viewedproducts">
-        <div ref="viewed" tabindex="0" class="viewed">
-          <br class="line-break">
-        </div>
-      </section>
-    </template>
-  </section>
 </template>
 
 <script>
-import carousel from 'vue-owl-carousel';
-import { mapState } from 'vuex';
+import carousel from "vue-owl-carousel";
+import { mapState } from "vuex";
 
 export default {
-  components: {
-    carousel
-  },
-  data() {
-    return {
-      // imageUrl: 'http://127.0.0.1:8000/images/',
-      imageUrl: 'https://joshgato-amazoned.herokuapp.com/images/',
-      settings: {
-        autoplay: true,
-        autoplayHoverPause: true,
-        nav: true,
-        "responsive": {0:{items:1,nav:false}, 600 :{items:3,nav:true}, 1000 :{items:6,nav:true}}
-      }
-    };
-  },
-  computed: {
-    ...mapState('viewedproducts', {
-      viewedproducts: state => state.viewedproducts
-    }),
-    ...mapState('products', {
-      products: state => state.products,
-    }),
-    ...mapState('categories', {
-      categories: state => state.categories,
-    })
-  },
-  methods: {
-    viewedProductsAnimation() {
-      const viewedSection = this.$refs['viewed-products'];
-      const viewedSectionPositionFromTop = viewedSection.offsetTop;
-      const viewedSectionHeight = viewedSection.offsetHeight;
-      // const viewed = this.$refs.viewed;
-      const viewed = document.querySelectorAll('.viewed');
-
-      const pageYOffset = window.pageYOffset;
-      const scrollTopValue = window.scrollY;
-
-      if (pageYOffset > (viewedSectionPositionFromTop - viewedSectionHeight * 99/100) ||
-        scrollTopValue > (viewedSectionPositionFromTop - viewedSectionHeight * 99/100)) {
-        viewed.forEach(element => {
-          element.classList.add('moveFromBottom');
-        });
-      } else {
-        viewed.forEach(element => {
-          element.classList.remove('moveFromBottom');
-        });
-      }
+    components: {
+        carousel
     },
-    getAttribute(productId, attributeName) {
-      const product = this.products.find(element => element.id === parseInt(productId));
-      return product[attributeName];
+    data() {
+        return {
+            // imageUrl: 'http://127.0.0.1:8000/images/',
+            //   imageUrl: 'https://joshgato-amazoned.herokuapp.com/images/',
+            imageUrl: process.env.MIX_IMAGE_URL,
+            settings: {
+                autoplay: true,
+                autoplayHoverPause: true,
+                nav: true,
+                responsive: {
+                    0: { items: 1, nav: false },
+                    600: { items: 3, nav: true },
+                    1000: { items: 6, nav: true }
+                }
+            }
+        };
     },
-    fetchOneCategoryMethod(id) {
-      const category = this.categories.find(element => element.id === parseInt(id));
-      return category.title;
+    computed: {
+        ...mapState("viewedproducts", {
+            viewedproducts: state => state.viewedproducts
+        }),
+        ...mapState("products", {
+            products: state => state.products
+        }),
+        ...mapState("categories", {
+            categories: state => state.categories
+        })
+    },
+    methods: {
+        viewedProductsAnimation() {
+            const viewedSection = this.$refs["viewed-products"];
+            const viewedSectionPositionFromTop = viewedSection.offsetTop;
+            const viewedSectionHeight = viewedSection.offsetHeight;
+            // const viewed = this.$refs.viewed;
+            const viewed = document.querySelectorAll(".viewed");
+
+            const pageYOffset = window.pageYOffset;
+            const scrollTopValue = window.scrollY;
+
+            if (
+                pageYOffset >
+                    viewedSectionPositionFromTop -
+                        (viewedSectionHeight * 99) / 100 ||
+                scrollTopValue >
+                    viewedSectionPositionFromTop -
+                        (viewedSectionHeight * 99) / 100
+            ) {
+                viewed.forEach(element => {
+                    element.classList.add("moveFromBottom");
+                });
+            } else {
+                viewed.forEach(element => {
+                    element.classList.remove("moveFromBottom");
+                });
+            }
+        },
+        getAttribute(productId, attributeName) {
+            const product = this.products.find(
+                element => element.id === parseInt(productId)
+            );
+            return product[attributeName];
+        },
+        fetchOneCategoryMethod(id) {
+            const category = this.categories.find(
+                element => element.id === parseInt(id)
+            );
+            return category.title;
+        }
+    },
+
+    mounted() {
+        window.addEventListener("scroll", this.viewedProductsAnimation);
+    },
+
+    destroyed() {
+        window.removeEventListener("scroll", this.viewedProductsAnimation);
     }
-  },
-
-  mounted() {
-    window.addEventListener('scroll', this.viewedProductsAnimation);
-  },
-
-  destroyed() {
-    window.removeEventListener('scroll', this.viewedProductsAnimation);
-  }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 $color: rgb(0, 112, 139);
 
 .animate__backInUp {
-  animation: backInUp;
-  animation-duration: 2s;
+    animation: backInUp;
+    animation-duration: 2s;
 }
 
 .moveFromBottom {
-  animation-name: moveFromBottom;
-  animation-duration: 2s;
-  animation-fill-mode: forwards;
-  // opacity: 1;
+    animation-name: moveFromBottom;
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
+    // opacity: 1;
 }
 
 @keyframes moveFromBottom {
-  from { opacity: 0; transform: translateY(90px); };
-  to { opacity: 1; transform: translateY(0); };
+    from {
+        opacity: 0;
+        transform: translateY(90px);
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
 }
 
 #viewed-products {
-  width: 100%;
-  padding: 3rem;
-  text-align: center;
-  font-family: 'Roboto', sans-serif;
+    width: 100%;
+    padding: 3rem;
+    text-align: center;
+    font-family: "Roboto", sans-serif;
 
-  .carousel-section {
-    
-  }
+    .carousel-section {
+    }
 
-  #carousel_mitaxbkq1hq {
-        
-    .owl-stage-outer {
-      width: 99%;
-      margin: 0 auto;
-      text-align: center;
+    #carousel_mitaxbkq1hq {
+        .owl-stage-outer {
+            width: 99%;
+            margin: 0 auto;
+            text-align: center;
 
-      .owl-stage {
-
-        .owl-item {
-          // display: flex !important;
-          // flex-direction: column;
-          // align-items: center !important;
-          // justify-content: center !important;
+            .owl-stage {
+                .owl-item {
+                    // display: flex !important;
+                    // flex-direction: column;
+                    // align-items: center !important;
+                    // justify-content: center !important;
+                }
+            }
         }
-      }
-    }
-  }
-
-  .section-heading {
-    margin-bottom: 0.5rem;
-
-    h3 {
-      width: 100%;
-      text-align: center;
-      font-size: 1.75rem;
-      font-weight: 600;
-      line-height: 1.2;
     }
 
-    span {
-      font-size: 14px;
-      color: rgba(0, 0, 0,0.5);
-      font-weight: 600;
-    }
-  }
+    .section-heading {
+        margin-bottom: 0.5rem;
 
-  .activeviewed {
-    height: 18rem;
-    width: 11rem !important;
-    margin: 0.5rem auto;
-
-    &:hover {
-      // border: 1px solid rgba(0, 0, 0, 0.1);
-      box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.1);
-      transform: translateY(-5px) !important;
-      transition: all 0.6s ease !important;
-
-      .details-container {
-        button {
-          opacity: 1;
+        h3 {
+            width: 100%;
+            text-align: center;
+            font-size: 1.75rem;
+            font-weight: 600;
+            line-height: 1.2;
         }
-      }
+
+        span {
+            font-size: 14px;
+            color: rgba(0, 0, 0, 0.5);
+            font-weight: 600;
+        }
     }
 
-    .img-container {
-      width: 100%;
-      height: 11rem;
-
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-
-    .details-container {
-      padding: 0 0.5rem;
-
-      .product-category {
-        font-size: 12px;
-        font-weight: 600;
-        color: rgba(0, 0, 0, 0.5);
-        padding: 0 0.3rem;
-        margin: auto;
-      }
-
-      .product-name {
-        font-size: 14px;
-        font-weight: 600;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-      }
-
-      .product-price {
-        font-weight: 600;
-        margin: auto;
-        color: #1f6475;
-      }
-
-      button {
-        border: none;
-        outline: none;
-        width: 90%;
-        color: #000;
-        background-color: $color;
-        opacity: 0;
+    .activeviewed {
+        height: 18rem;
+        width: 11rem !important;
+        margin: 0.5rem auto;
 
         &:hover {
-          color: #fff;
-          background-color: darken($color, 15%);
-        }
-      }
-    }
-  }
+            // border: 1px solid rgba(0, 0, 0, 0.1);
+            box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.1);
+            transform: translateY(-5px) !important;
+            transition: all 0.6s ease !important;
 
-  .line-break {
-    margin: 2rem 0;
-  }
+            .details-container {
+                button {
+                    opacity: 1;
+                }
+            }
+        }
+
+        .img-container {
+            width: 100%;
+            height: 11rem;
+
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        .details-container {
+            padding: 0 0.5rem;
+
+            .product-category {
+                font-size: 12px;
+                font-weight: 600;
+                color: rgba(0, 0, 0, 0.5);
+                padding: 0 0.3rem;
+                margin: auto;
+            }
+
+            .product-name {
+                font-size: 14px;
+                font-weight: 600;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+            }
+
+            .product-price {
+                font-weight: 600;
+                margin: auto;
+                color: #1f6475;
+            }
+
+            button {
+                border: none;
+                outline: none;
+                width: 90%;
+                color: #000;
+                background-color: $color;
+                opacity: 0;
+
+                &:hover {
+                    color: #fff;
+                    background-color: darken($color, 15%);
+                }
+            }
+        }
+    }
+
+    .line-break {
+        margin: 2rem 0;
+    }
 }
 </style>
--- resources/js/components/ProductPage/ProductPage.vue
+++ resources/js/components/ProductPage/ProductPage.vue
@@ -1,118 +1,120 @@
 <template>
-  <section id="product-page">
-    <div id="product-details">
-      <section class="image-section">
-        <img :src="imageUrl + product.image" :alt="product.title">
-      </section>
-      <section class="details-section">
-        <h4 class="product-title">{{ product.title }}</h4>
-        <hr>
-        <h1 class="product-price">$ {{ product.price }}</h1>
-        <hr>
-          <h5 class="product-description-heading">Product Details</h5>
-          <p class="product-description">{{ product.description }}</p>
-        <hr>
-        
-      </section>
-    </div>
-  </section>
+    <section id="product-page">
+        <div id="product-details">
+            <section class="image-section">
+                <img :src="imageUrl + product.image" :alt="product.title" />
+            </section>
+            <section class="details-section">
+                <h4 class="product-title">{{ product.title }}</h4>
+                <hr />
+                <h1 class="product-price">$ {{ product.price }}</h1>
+                <hr />
+                <h5 class="product-description-heading">Product Details</h5>
+                <p class="product-description">{{ product.description }}</p>
+                <hr />
+            </section>
+        </div>
+    </section>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
-
+import { mapState, mapActions } from "vuex";
 export default {
-  // props: ['productid'],
-  data() {
-    return {
-      // imageUrl: 'http://127.0.0.1:8000/images/',
-      imageUrl: 'https://joshgato-amazoned.herokuapp.com/images/',
-    }
-  },
-  methods: {
-    ...mapActions({
-      fetchOneProductAction: 'products/fetchOneProduct',
-      addToViewedProductsAction: 'viewedproducts/addToViewedProducts',
-      fetchViewedProductsAction: 'viewedproducts/fetchViewedProducts'
-    }),
-    fetchOneProduct(id) {
-      this.fetchOneProductAction(id);
-    }
-  },
-  computed: {
-    ...mapState('products', {
-      product: state => state.product,
-    }),
-    ...mapState('cart', {
-      uuid: state => state.uuid,
-    })
-  },
+    // props: ['productid'],
+    data() {
+        return {
+            // imageUrl: 'http://127.0.0.1:8000/images/',
+            //   imageUrl: 'https://joshgato-amazoned.herokuapp.com/images/',
+            imageUrl: process.env.MIX_IMAGE_URL
+        };
+    },
+    methods: {
+        ...mapActions({
+            fetchOneProductAction: "products/fetchOneProduct",
+            addToViewedProductsAction: "viewedproducts/addToViewedProducts",
+            fetchViewedProductsAction: "viewedproducts/fetchViewedProducts"
+        }),
+        fetchOneProduct(id) {
+            this.fetchOneProductAction(id);
+        }
+    },
+    computed: {
+        ...mapState("products", {
+            product: state => state.product
+        }),
+        ...mapState("cart", {
+            uuid: state => state.uuid
+        })
+    },
 
-  created() {
-    const productId = this.$route.params.id;
-    const uuid = this.uuid;
+    created() {
+        const productId = this.$route.params.id;
+        const uuid = this.uuid;
 
-    this.fetchOneProduct(productId);
-    this.addToViewedProductsAction({ uuid, product: productId });
-    this.fetchViewedProductsAction(uuid);
-  },
-}
+        this.fetchOneProduct(productId);
+        this.addToViewedProductsAction({ uuid, product: productId });
+        this.fetchViewedProductsAction(uuid);
+    }
+};
 </script>
 
 <style lang="scss" scoped>
 #product-page {
-  background: linear-gradient(rgba(228, 228, 228, 0.5), rgba(219, 219, 219, 0.3));
-  font-family: 'Roboto';
-  padding-top: 5rem;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-
-  #product-details {
-    background-color: #fff;
-    // width: 100%;
-    height: 20rem;
+    background: linear-gradient(
+        rgba(228, 228, 228, 0.5),
+        rgba(219, 219, 219, 0.3)
+    );
+    font-family: "Roboto";
+    padding-top: 5rem;
+    width: 100%;
+    height: 100vh;
     display: flex;
-    // padding: 1rem 0.5rem; //this line couldnt distribute the padding equally [findout later]
+    justify-content: center;
 
-    .image-section {
-      width: 20rem;
-      height: 20rem;
-      padding: 1rem 2rem;
+    #product-details {
+        background-color: #fff;
+        // width: 100%;
+        height: 20rem;
+        display: flex;
+        // padding: 1rem 0.5rem; //this line couldnt distribute the padding equally [findout later]
 
-      img {
-        width: 100%;
-        height: 100%;
-      }
+        .image-section {
+            width: 20rem;
+            height: 20rem;
+            padding: 1rem 2rem;
+
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        .details-section {
+            padding: 1rem 2rem;
+            width: 40rem;
+            height: 20rem;
+
+            .product-title {
+                font-size: 20px;
+                color: #444444;
+                font-weight: 400;
+                -webkit-font-smoothing: antialiased;
+            }
+
+            .product-price {
+                color: #505050;
+                font-size: 28px;
+            }
+
+            .product-description-heading {
+                color: #464646;
+            }
+
+            .product-description {
+                color: #818181;
+                font-size: 14px;
+            }
+        }
     }
-
-    .details-section {
-      padding: 1rem 2rem;
-      width: 40rem;
-      height: 20rem;
-
-      .product-title {
-        font-size: 20px;
-        color: #444444;
-        font-weight: 400;
-        -webkit-font-smoothing: antialiased;
-      }
-
-      .product-price {
-        color: #505050;
-        font-size: 28px;
-      }
-
-      .product-description-heading {
-        color: #464646;
-      }
-
-      .product-description {
-        color: #818181;
-        font-size: 14px;
-      }
-    }
-  }
 }
 </style>
